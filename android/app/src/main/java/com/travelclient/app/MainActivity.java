2e4071bbad21bb2ac2ee2757cd0ecf9fd6c452f9
shtinberg <shtinberg@citycar.co.il>
2026-09-16 16:42:54 +0300

    add Capacitor Android app

---
+++ android/app/src/main/java/com/travelclient/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.travelclient.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.travelclient.app',
+  appName: 'travel_client',
+  webDir: 'dist',
+};
+
+export default config;
